2bcca107094d1005c3f306d85cfb22f003de8d11
Кіра Боціон <botsionkira2006@gmail.com>
2025-02-19 13:26:37 +0200

    test menu

--- main.py
+++ main.py
@@ -105,44 +105,6 @@ class Menu:
         BG_COLOR = self.bg_colors[self.bg_color]
 
 # --- Класи гри ---
-class GameSettings:
-    def __init__(self):
-        self.difficulty = 'medium'
-        self.bg_color = 'black'
-        self.parse_args()
-
-    def parse_args(self):
-        parser = argparse.ArgumentParser(description="Pac-Man Game Settings")
-        parser.add_argument('--difficulty', type=str, choices=['easy', 'medium', 'hard'], default='hard', help="Set the game difficulty")
-        parser.add_argument('--bg_color', type=str, choices=['black', 'pink', 'gray'], default='pink', help="Set the background color")
-        args = parser.parse_args()
-        self.difficulty = args.difficulty
-        self.bg_color = args.bg_color
-
-        # Set delays based on difficulty
-        global pacman_move_delay, ghost_move_delay, mouth_anim_delay
-        if self.difficulty == 'easy':      
-            pacman_move_delay = 200
-            ghost_move_delay = 500
-            mouth_anim_delay = 600
-        elif self.difficulty == 'medium':
-            pacman_move_delay = 150
-            ghost_move_delay = 400
-            mouth_anim_delay = 500
-        else:  # 'hard' 
-            pacman_move_delay = 100
-            ghost_move_delay = 300
-            mouth_anim_delay = 400
-
-        # Set background color
-        global BG_COLOR
-        if self.bg_color == 'pink':
-            BG_COLOR = PINK
-        elif self.bg_color == 'gray':
-            BG_COLOR = GRAY
-        else:  # 'default(black)'
-            BG_COLOR = BLACK
-
 class PacMan:
     def __init__(self):
         self.x, self.y, self.direction = 1, 1, 3
